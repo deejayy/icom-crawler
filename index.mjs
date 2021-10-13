fe2a41df7ed11c7ed44d758aec2ef11e9dd6fd13
deejayy <deejayy@deejayy.hu>
2021-10-13 23:46:40 +0200

    Introduce scoring

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import fetch from "node-fetch";
 import fs from "fs";
 import jsdom from "jsdom";
 import cities from "./cities.mjs";
+import cityScores from "./city-scores.mjs";
 
 String.prototype.strip = function () {
   this.toLowerCase()
@@ -96,6 +97,7 @@ const parseListItem = (item) => {
   ];
   const { city, street } = parseAddress(item.querySelector(".listing__address").textContent.trim());
   return {
+    score: 0,
     id: item.getAttribute("data-id"),
     url: `https://ingatlan.com${item.querySelector(".listing__thumbnail").getAttribute("href")}`,
     pic: item.querySelector(".listing__image").getAttribute("src"),
@@ -143,11 +145,59 @@ const toCsv = (table) => {
   return csv;
 };
 
+const scoreTable = {
+  "Ingatlan állapota": {
+    "felújítandó": -50,
+    "felújított": 0,
+    "jó állapotú": 0,
+    "közepes állapotú": -30,
+    "nincs megadva": 0,
+    "új építésű": 10,
+    "újszerű": 0,
+    "default": -20,
+  },
+  "Építés éve": {
+    "1950 előtt": -50,
+    "1950 és 1980 között": -30,
+    "1981 és 2000 között": -20,
+    "2001 és 2010 között": 0,
+    "2012": 2,
+    "2013": 3,
+    "2015": 5,
+    "2016": 6,
+    "2017": 7,
+    "2018": 8,
+    "2019": 9,
+    "2020": 10,
+    "2021": 11,
+    "nincs megadva": 0,
+    "default": 0,
+  },
+};
+
+const scoring = (item) => {
+  const score = [
+    (40 - item.price) / 2,
+    (10 - (Math.abs(item.area - 80) / 100) * 10) * 1,
+    (10 - (Math.abs(item.plot - 600) / 600) * 7) * 1,
+    item.fullRooms >= 3 ? 5 + item.fullRooms / 2 - 2 + item.halfRooms / 2 : -20,
+    cityScores[item.city] || 0,
+    ...Object.keys(scoreTable).map((scoreCat) =>
+      item[scoreCat] ? scoreTable[scoreCat][item[scoreCat]] || scoreTable[scoreCat]["default"] : 0,
+    ),
+  ].reduce((acc, curr) => acc + (curr || 0), 0);
+
+  return {
+    ...item,
+    score,
+  };
+};
+
 async function getPage(page) {
   if (!page) return;
 
   const body = await download(
-    'list',
+    "list",
     `https://ingatlan.com/lista/elado+haz+80-m2-felett+csak-kepes+pest-megye-buda-kornyeke+pest-megye-pest-kornyeke+budapest+pest-megye+budapest-pesti-oldal+budapest-budai-oldal+850-m2telek-alatt+csaladi-haz+konnyuszerkezetes-haz+3-szoba-felett+45-mFt-ig?page=${page}`,
   );
   let html = new jsdom.JSDOM(body);
@@ -155,7 +205,7 @@ async function getPage(page) {
   const result = items.map(parseListItem);
 
   for (let i = 0; i < result.length; i++) {
-    const advert = await download('match', result[i].url);
+    const advert = await download("match", result[i].url);
     let advertHtml = new jsdom.JSDOM(advert);
     const parameters = [...advertHtml.window.document.querySelector("dl.parameters").querySelectorAll(".parameter")];
     const paramObject = parameters.map(parseParams).reduce(convertParamsReducer, {});
@@ -177,13 +227,22 @@ async function getPage(page) {
   let counter = 1;
   let result = [];
   let singleResult;
-  do {
-    singleResult = await getPage(counter++);
-    result = [...result, ...singleResult];
-    await new Promise((resolve) => setTimeout(resolve, 500)).then(() => console.log('Delayed'));
-  } while (singleResult?.length >= 20);
 
-  // const result = [...(await getPage(1)), ...(await getPage(2))];
+  try {
+    result = JSON.parse(fs.readFileSync('./result.json', 'utf-8'));
+  } catch {
+    result = [];
+  }
+
+  if (result.length === 0) {
+    do {
+      singleResult = await getPage(counter++);
+      result = [...result, ...singleResult];
+      await new Promise((resolve) => setTimeout(resolve, 500)).then(() => console.log("Delayed"));
+    } while (singleResult?.length >= 20);
+  }
+
+  result = result.map(scoring).sort((a, b) => (a.score > b.score ? -1 : 1));
   const csv = toCsv(result);
 
   fs.writeFileSync("result.json", JSON.stringify(result, null, 2));
